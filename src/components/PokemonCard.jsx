29e9218e18f240aa5c7a7b9b359366400c1ada00
alucart2005 <napoleonanaya@gmail.com>
2023-11-30 21:07:58 -0500

    feature: end of the project

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -21,6 +21,31 @@ export function PokemonCard({
             <p>{name}</p>
             <img src={image} alt={name} className="img-litle" />
           </div>
+          <img src={image} alt={name} />
+          <div style={{ display: "flex", width: "100%" }}>
+            <div className="stats-left">
+              <p>type</p>
+              <p>weight</p>
+              <p>height</p>
+            </div>
+            <div className="stats-right" style={{ background: "purple" }}>
+              <p>{type}</p>
+              <p>{weight} Lbs</p>
+              <p>{height}0 Cm</p>
+            </div>
+          </div>
+          <div className="base-stats">
+            <div>
+              {statsName.map((stats) => (
+                <div className="stats">{stats}</div>
+              ))}
+            </div>
+            <div>
+              {stats.map((stats) => (
+                <p className="stats">{ stats}</p>
+              ))}
+            </div>
+          </div>
         </div>
       )}
       <div
@@ -37,7 +62,7 @@ export function PokemonCard({
             width: "50px",
           }}
         />
-        <p style={{ width: "270px"}}>No. {id}</p>
+        <p style={{ width: "270px" }}>No. {id}</p>
         <p>{name}</p>
         <img
           src={pokeball}
